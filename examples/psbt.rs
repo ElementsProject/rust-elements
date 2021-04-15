7cbccbf8c7e29a764d64dff407c916185d032a86
sanket1729 <sanket1729@gmail.com>
2021-04-15 15:53:13 -0700

    add pset workflow exmaple

---
+++ examples/psbt.rs
@@ -0,0 +1,11 @@
+extern crate elements;
+
+use elements::pset::PartiallySignedTransaction;
+use elements::secp256k1_zkp;
+/// Pset example workflow:
+/// Simple transaction spending a confidential asset
+/// with external signer and an external blinder
+
+fn main(){
+
+}
