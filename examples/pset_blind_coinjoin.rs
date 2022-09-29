ecb2c3091b7a2e6b9a287563f6ab13e060587121
Riccardo Casatta <riccardo@casatta.it>
2022-09-29 17:07:21 +0200

    Use crappy rng for pset_blind_coinjoin, update test_vector

--- examples/pset_blind_coinjoin.rs
+++ examples/pset_blind_coinjoin.rs
@@ -27,7 +27,6 @@ use elements::{pset, secp256k1_zkp};
 use elements::encode::{deserialize, serialize_hex};
 use elements::hashes::hex::FromHex;
 use elements::{confidential, AssetId, TxOut};
-use rand::SeedableRng;
 
 // Assume txouts are simple pay to wpkh
 // and keep the secrets correponding to
@@ -138,8 +137,7 @@ fn main() {
     let tests = test_data();
     // Initially secp context and rng global state
     let secp = secp256k1_zkp::Secp256k1::new();
-    #[allow(deprecated)]
-    let mut rng = rand::ChaChaRng::seed_from_u64(0);
+    let mut rng = CrappyRng::new(core::num::NonZeroU64::new(1).unwrap());
 
     let txouts = txout_data();
     let (btc_txout, btc_txout_secrets, btc_inp) = txouts[0].clone();
@@ -287,3 +285,43 @@ fn main() {
     tx.verify_tx_amt_proofs(&secp, &[btc_txout, asset_txout])
         .unwrap();
 }
+
+
+/// Xorshift
+pub struct CrappyRng(u64);
+
+impl CrappyRng {
+    fn new(initial: core::num::NonZeroU64) -> Self {
+        Self(initial.get())
+    }
+}
+
+impl rand::RngCore for CrappyRng {
+
+    fn next_u32(&mut self) -> u32 {
+        self.next_u64() as u32
+    }
+
+    fn next_u64(&mut self) -> u64 {
+        let mut x = self.0;
+        x ^= x << 13;
+        x ^= x >> 7;
+        x ^= x << 17;
+        self.0 = x;
+        x
+      }
+
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        for chunk in dest.chunks_mut(8) {
+            let x = self.next_u64().to_be_bytes();
+            chunk.copy_from_slice(&x[..chunk.len()]);
+
+        }
+    }
+
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand::Error> {
+        Ok(self.fill_bytes(dest))
+    }
+}
+
+impl rand::CryptoRng for CrappyRng {}
