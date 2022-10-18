ee76903b753c8a103c81aa0208246deb42fe529f
Andrew Poelstra <apoelstra@wpsoftware.net>
2022-10-18 19:24:14 +0000

    locktime: update comment slightly for Elements

--- src/locktime.rs
+++ src/locktime.rs
@@ -34,10 +34,10 @@ use crate::parse::impl_parse_str_through_int;
 /// `LockTime` values _below_ the threshold are interpreted as block heights, values _above_ (or
 /// equal to) the threshold are interpreted as block times (UNIX timestamp, seconds since epoch).
 ///
-/// Bitcoin is able to safely use this value because a block height greater than 500,000,000 would
-/// never occur because it would represent a height in approximately 9500 years. Conversely, block
+/// Elements is able to safely use this value because a block height greater than 500,000,000 would
+/// never occur because it would represent a height in approximately 950 years. Conversely, block
 /// times under 500,000,000 will never happen because they would represent times before 1986 which
-/// are, for obvious reasons, not useful within the Bitcoin network.
+/// are, for obvious reasons, not useful within any Elements network.
 ///
 /// [Bitcoin Core]: https://github.com/bitcoin/bitcoin/blob/9ccaee1d5e2e4b79b0a7c29aadb41b97e4741332/src/script/script.h#L39
 pub const LOCK_TIME_THRESHOLD: u32 = 500_000_000;
