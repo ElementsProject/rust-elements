802c0f496f2a9691c87de7a95f21e40180ab9840
Riccardo Casatta <riccardo@casatta.it>
2023-07-19 11:38:51 +0200

    pin serde_json for 1.48.0

--- contrib/test.sh
+++ contrib/test.sh
@@ -4,6 +4,7 @@ FEATURES="serde"
 
 # Pin dependencies as required if we are using MSRV toolchain.
 if cargo --version | grep "1\.48"; then
+    cargo update -p serde_json --precise 1.0.99
     # 1.0.157 uses syn 2.0 which requires edition 2018
     cargo update -p serde --precise 1.0.156
 fi
